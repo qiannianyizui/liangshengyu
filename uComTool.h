eb0256fd35c910dd8ebb979a0066411c640afc71
liangshengyu <2057890372@qq.com>
2018-09-22 01:21:34 +0800

    init commit

---
+++ uComTool.h
@@ -0,0 +1,22 @@
+#ifndef UCOMTOOL_H
+#define UCOMTOOL_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // UCOMTOOL_H
